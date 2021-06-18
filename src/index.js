5ef3935b0df9b00deb12db24f15efe22041cbd5e
luis-luciqm <redbullfc18@gmail.com>
2021-06-17 21:46:40 -0300

    FinAPI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-
+app.post("/account", (req, res) => {
+    const cpf = req.body; // pegando cpf de um form
+});
 
 app.listen(5000, () => {
     console.log('server is running in port 5000');

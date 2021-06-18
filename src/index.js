703c4ef38dd9e46d6a1f6bb2bf0d3558e42d404d
luis-luciqm <redbullfc18@gmail.com>
2021-06-17 23:06:34 -0300

    FinAPI - verificando se cpf existe no database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid') // v4 gera números randomicos para o id
+
+app.use(express.json()); // middleware para formatos json
+
+customers = [];
 
 app.post("/account", (req, res) => {
-    const cpf = req.body; // pegando cpf de um form
+    const {cpf, name} = req.body; // desestruturação
+
+    const existeUmCpf = customers.some( // verifica se existe aquele cpf já cadastrado
+        (customer) => customer.cpf === cpf
+    );
+
+    if (existeUmCpf){
+        return res.status(400).json({error: "Esse cpf já existe"});
+    }
+
+    customers.push({ // salvando no array (database fake)
+        id: uuidv4(),
+        name,
+        cpf,
+        statement: [],
+    });
+
+    return res.status(201).send();
+
+});
+
+app.get("/statement/:cpf", (req, res) => { // pegando cpf via route params  
+    const { cpf } = req.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    return res.json(customer.statement)
+
 });
 
 app.listen(5000, () => {

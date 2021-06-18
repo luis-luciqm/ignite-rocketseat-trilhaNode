a5c8dc6f2c5aa7e936655450b92c8637fa96b1e5
luis-luciqm <redbullfc18@gmail.com>
2021-06-17 21:42:13 -0300

    FinAPI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({message: 'Welcome to Ignite!'});
-})
+
 
 app.listen(5000, () => {
     console.log('server is running in port 5000');
-})
+});
